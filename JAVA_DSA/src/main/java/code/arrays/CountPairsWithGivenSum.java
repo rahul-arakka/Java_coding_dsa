c10215a4cbd10b7759741fe25dd0292893b0d325
rahul-arakka <arakkarahul11@gmail.com>
2024-07-03 00:06:43 +0530

    CountPairsWithGivenSum GFG Java solution - optimized approach - All test cases passed.

--- JAVA_DSA/src/main/java/code/arrays/CountPairsWithGivenSum.java
+++ JAVA_DSA/src/main/java/code/arrays/CountPairsWithGivenSum.java
@@ -4,17 +4,46 @@
 
 package code.arrays;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CountPairsWithGivenSum {
     int getPairsCount(int[] arr, int sum) {
         // code here
-        int pairs=0;
-        for(int i=0;i<arr.length;i++){
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i] + arr[j] == sum){
-                    pairs++;
-                }
+//        int pairs=0;
+//        for(int i=0;i<arr.length;i++){
+//            for(int j=i+1;j<arr.length;j++){
+//                if(arr[i] + arr[j] == sum){
+//                    pairs++;
+//                }
+//            }
+//        }
+//        return pairs;
+
+        //Optimized version
+        // Initialize count of pairs
+        int pairs = 0;
+
+        // Map to store frequency of each element
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+
+        // Traverse through the array
+        for (int i = 0; i < arr.length; i++) {
+            // Calculate the complement required to reach the sum
+            int complement = sum - arr[i];
+
+            // If complement exists in the map, it means we have found pairs
+            if (frequencyMap.containsKey(complement)) {
+                pairs += frequencyMap.get(complement);
             }
+
+            // Update the frequency of the current element in the map
+            frequencyMap.put(arr[i], frequencyMap.getOrDefault(arr[i], 0) + 1);
         }
+
+        // Return the total count of pairs
         return pairs;
+
+
     }
 }
