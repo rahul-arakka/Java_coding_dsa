f564a799c3234aad8b6045c26909a18f40161556
rahul-arakka <arakkarahul11@gmail.com>
2024-07-03 23:31:54 +0530

    KeyPair GFG Java solution - Alternate solution

--- JAVA_DSA/src/main/java/code/arrays/KeyPair.java
+++ JAVA_DSA/src/main/java/code/arrays/KeyPair.java
@@ -7,6 +7,7 @@ package code.arrays;
 public class KeyPair {
     boolean hasArrayTwoCandidates(int arr[], int x) {
         // code here
+        // Basic approach - 0.9 sec
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[i] + arr[j] == x)
@@ -14,5 +15,25 @@ public class KeyPair {
             }
         }
         return false;
+
+
+        // Time Taken is longer in below approach  - 1.51 sec
+
+        // Arrays.sort(arr);
+        // int index = arr.length;
+        // for(int i=0;i<arr.length;i++){
+        //     if(arr[i] > x){
+        //         index = i;
+        //         break;
+        //     }
+        // }
+
+        // for(int i=0;i<index-1;i++){
+        //     for(int j=i+1;j<index;j++){
+        //         if(arr[i] + arr[j] == x){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
     }
-}
