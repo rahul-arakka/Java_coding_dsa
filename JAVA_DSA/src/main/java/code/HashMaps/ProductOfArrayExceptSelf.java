94ae6d8578f9562845b3a909adfd3a170a91fed7
rahul-arakka <arakkarahul11@gmail.com>
2024-08-02 23:40:29 +0530

    ProductOfArrayExceptSelf - Optimized solution | Java DSA | Leetcode

--- JAVA_DSA/src/main/java/code/HashMaps/ProductOfArrayExceptSelf.java
+++ JAVA_DSA/src/main/java/code/HashMaps/ProductOfArrayExceptSelf.java
@@ -8,32 +8,45 @@ package code.HashMaps;
 class Solution {
 
 //    Solution with division operation used
-    public int[] productExceptSelf(int[] nums) {
-        int[] res = new int[nums.length];
-        int countZero = 0, indexZero = -1, sum = 1;
+//    public int[] productExceptSelf(int[] nums) {
+//        int[] res = new int[nums.length];
+//        int countZero = 0, indexZero = -1, sum = 1;
+//
+//        for (int i = 0; i < nums.length; i++) {
+//            if (nums[i] == 0) {
+//                nums[i] = 1;
+//                countZero++;
+//                indexZero = i;
+//            }
+//
+//            sum *= nums[i];
+//        }
+//
+//        if (countZero > 0) {
+//            for (int i = 0; i < nums.length; i++) {
+//                res[i] = 0;
+//            }
+//        } else {
+//            for (int j = 0; j < nums.length; j++) {
+//                res[j] = sum / nums[j];
+//            }
+//        }
+//        if (indexZero != -1 && countZero == 1)
+//            res[indexZero] = sum;
+//
+//        return res;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0) {
-                nums[i] = 1;
-                countZero++;
-                indexZero = i;
-            }
 
-            sum *= nums[i];
-        }
+     int lsum = 1, rsum = 1;
+     for(int i=0;i<nums.length;i++){
+         res[i] = lsum;
+         lsum *= nums[i];
+     }
+     for(int i=nums.length-1;i>=0;i--){
+         res[i] *= rsum;
+         rsum *= nums[i];
+     }
 
-        if (countZero > 0) {
-            for (int i = 0; i < nums.length; i++) {
-                res[i] = 0;
-            }
-        } else {
-            for (int j = 0; j < nums.length; j++) {
-                res[j] = sum / nums[j];
-            }
-        }
-        if (indexZero != -1 && countZero == 1)
-            res[indexZero] = sum;
-
-        return res;
+     return res;
     }
 }
