a84da38848772595a51bd0c53e4f31d1bab63acf
rahul-arakka <arakkarahul11@gmail.com>
2024-08-07 22:09:44 +0530

    TopKFrequestElements - optimized approach - all test cases passed | Java DSA | Leetcode

--- JAVA_DSA/src/main/java/code/HashMaps/TopKFrequestElements.java
+++ JAVA_DSA/src/main/java/code/HashMaps/TopKFrequestElements.java
@@ -5,34 +5,29 @@
 package code.HashMaps;
 
 class TopKFrequestElements {
-    class Solution {
-        public int[] topKFrequent(int[] nums, int k) {
-            int[] res = new int[k];
-            int x = 0;
-            Map<Integer, Integer> map = new HashMap<>();
-            for (int key : nums) {
-                int count = map.containsKey(key) ? map.get(key) : 0;
-                map.put(key, count + 1);
-
-            }
-            ArrayList<Integer> rmv = new ArrayList<>();
-            // System.out.println("- Printing Map : " + map);
-            for (int i = 0; i < k; i++) {
-                int max = 0, ans = 0;
-                for (Map.Entry val : map.entrySet()) {
-                    if ((int) val.getValue() > max && !rmv.contains((int) val.getKey())) {
-                        max = (int) val.getValue();
-                        ans = (int) val.getKey();
-                        rmv.add(ans);
-
-
-                        // System.out.println(" Printing ans, max: " + ans + "," + max);
-                    }
-                }
-                res[x] = ans;
-                x++;
-            }
-            return res;
-        }
+    int[] res = new int[k];
+    Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+        map.put(nums[i], map.getOrDefault(nums[i],0)+1);
     }
+    List<Integer> list[] = new ArrayList[nums.length+1];
+        for(Map.Entry ent : map.entrySet()){
+        if(list[(int)(ent.getValue())] == null){
+            list[(Integer)(ent.getValue())] = new ArrayList<>();
+        }
+        list[(Integer)(ent.getValue())].add((Integer)(ent.getKey()));
+
+    }
+    int x = 0;
+        for(int j=nums.length;j>=0;j--){
+        if(list[j] != null){
+            for(int val : list[j]){
+                res[x++] = val;
+            }
+            if(x == k)   return res;
+        }
+
+    }
+    // System.out.println("- Printing arraylist : " + list);
+        return res;
 }
