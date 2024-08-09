8efb454524e37b43c60edf51bba1c4ac77654f85
rahul-arakka <arakkarahul11@gmail.com>
2024-08-09 23:32:57 +0530

    SearchInsertPosition - All test cases passed - time and memory updated | Java DSA | Leetcode

--- JAVA_DSA/src/main/java/code/arrays/SearchInsertPosition.java
+++ JAVA_DSA/src/main/java/code/arrays/SearchInsertPosition.java
@@ -2,6 +2,15 @@
 //Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 //You must write an algorithm with O(log n) runtime complexity.
 
+// Runtiime
+// Time taken: 0 ms
+// Beats: 100%
+
+//Memory
+//42.91MB
+// beats 32.47%.
+
+
 package code.arrays;
 
 class SearchInsertPosition {
