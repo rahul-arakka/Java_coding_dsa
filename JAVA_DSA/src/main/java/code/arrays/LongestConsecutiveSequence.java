86189d0f33b88ab9058a4cda40ed008b42d7ed46
Rahul Arakka <arakkarahul11@gmail.com>
2024-08-14 00:12:20 +0530

    LongestConsecutiveSequence - All test cases passed | Leetcode | Optimal solution

--- JAVA_DSA/src/main/java/code/arrays/LongestConsecutiveSequence.java
+++ JAVA_DSA/src/main/java/code/arrays/LongestConsecutiveSequence.java
@@ -3,6 +3,8 @@
 //Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
 //You must write an algorithm that runs in O(n) time.
 
+//Optimal solution better than the solution provided as answer.
+
 //This solution is valid for array contains duplicate or non duplicate elements both - Enhanced verison
 
 package code.arrays;
@@ -12,26 +14,18 @@ class LongestConsecutiveSequence {
         public int longestConsecutive(int[] nums) {
             if (nums.length == 0) return 0;
             Arrays.sort(nums);
-            int count = 1, prevCount = 1;
+            int count = 1, max = 1;
             for (int i = 0; i < nums.length - 1; i++) {
-                if (nums[i] == nums[i + 1])
-                    continue;
-                if (nums[i + 1] == nums[i] + 1) {
+                if (nums[i] == nums[i + 1]) continue;
+                if (nums[i] + 1 == nums[i + 1]) {
                     count++;
-                } else {
-                    if (count > prevCount) {
-                        prevCount = count;
-                    }
-                    // prevCount = count;
+                    max = Math.max(max, count);
+                } else
                     count = 1;
-                    // value = nums[i];
-                }
+            }
 
-            }
-            if (count > prevCount) {
-                return count;
-            }
-            return prevCount;
+
+            return max;
         }
     }
 }
