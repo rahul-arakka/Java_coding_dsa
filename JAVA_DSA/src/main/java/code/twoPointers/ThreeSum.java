900acfade386fff6caa0405f86b7b11eac8b32a0
Rahul Arakka <arakkarahul11@gmail.com>
2024-08-14 17:47:06 +0530

    ThreeSum - All test cases passed | Leetcode | Optimal solution

--- JAVA_DSA/src/main/java/code/twoPointers/ThreeSum.java
+++ JAVA_DSA/src/main/java/code/twoPointers/ThreeSum.java
@@ -3,51 +3,42 @@
 //Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
 //Notice that the solution set must not contain duplicate triplets.
 
+// This is optimal approach, all test cases are passed.
+
 package code.twoPointers;
 
 class ThreeSum {
-
-    class Solution {class Solution {
-
-        //2 pointers
+    class Solution {
         public List<List<Integer>> threeSum(int[] nums) {
+            List<List<Integer>> ans = new ArrayList<>();
+            List<Integer> dup = new ArrayList<>();
             Arrays.sort(nums);
-            LinkedList<List<Integer>> sol = new LinkedList<List<Integer>>();
-
             for (int i = 0; i < nums.length - 2; i++) {
-                //Only consider non-duplicate elements for i
-                if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+                if (i == 0 || i > 0 && nums[i] != nums[i - 1]) {
                     int target = 0 - nums[i];
-                    int left = i + 1;
-                    int right = nums.length - 1;
+                    int l = i + 1, r = nums.length - 1;
+                    while (l < r) {
+                        if (nums[l] + nums[r] == target) {
+                            List<Integer> list = new ArrayList<>();
+                            list.add(nums[i]);
+                            list.add(nums[l]);
+                            list.add(nums[r]);
+                            ans.add(list);
 
-                    while (left < right) {
-                        if (nums[left] + nums[right] == target) {
-                            ArrayList<Integer> miniSol = new ArrayList<>();
-                            miniSol.add(nums[i]);
-                            miniSol.add(nums[left]);
-                            miniSol.add(nums[right]);
-                            sol.add(miniSol);
-                            while (left < right && nums[left] == nums[left + 1]) {
-                                left++;
-                            }
-                            while (left < right && nums[right] == nums[right - 1]) {
-                                right--;
-                            }
-                            left++;
-                            right--;
-                        } else if (nums[left] + nums[right] > target) {
-                            right--;
-                        } else {
-                            left++;
+                            while (l < r && nums[l] == nums[l + 1]) l++;
+                            while (l < r && nums[r] == nums[r - 1]) r--;
+                            l++;
+                            r--;
+
+                        } else if (nums[l] + nums[r] < target) {
+                            l++;
+                        } else if (nums[l] + nums[r] > target) {
+                            r--;
                         }
                     }
                 }
             }
-
-            return sol;
+            return ans;
         }
     }
-
-
-    }
+}
