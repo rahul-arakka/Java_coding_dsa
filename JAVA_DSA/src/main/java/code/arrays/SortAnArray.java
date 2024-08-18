5d03398dadd990d538f12030575907ea6a838517
Rahul Arakka <arakkarahul11@gmail.com>
2024-08-19 01:19:19 +0530

    SortAnArray - All test cases passed | Leetcode | Arrays - Medium | Merge Sort

--- JAVA_DSA/src/main/java/code/arrays/SortAnArray.java
+++ JAVA_DSA/src/main/java/code/arrays/SortAnArray.java
@@ -3,24 +3,54 @@
 //Given an array of integers nums, sort the array in ascending order and return it.
 //You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
 
-// This approach has time complexity on O(n2) and uses contant space (temp variable).
+// This approach is called MERGE SORT & has time complexity of O(nlogn) and uses space 'N' (int array 'merge').
 
 package code.arrays;
 
 public class SortAnArray {
     class Solution {
+        public static void conquor(int[] nums, int si, int mid, int ei) {
+
+            int[] merge = new int[ei - si + 1];
+
+            int idx1 = si;
+            int idx2 = mid + 1;
+            int x = 0;
+            while (idx1 <= mid && idx2 <= ei) {
+                if (nums[idx1] <= nums[idx2]) {
+                    merge[x++] = nums[idx1++];
+                } else {
+                    merge[x++] = nums[idx2++];
+                }
+            }
+
+            while (idx1 <= mid) {
+                merge[x++] = nums[idx1++];
+            }
+            while (idx2 <= ei) {
+                merge[x++] = nums[idx2++];
+            }
+            for (int i = 0, j = si; i < merge.length; i++, j++) {
+                nums[j] = merge[i];
+            }
+
+        }
+
+        public static void divide(int[] nums, int si, int ei) {
+            if (si >= ei) {
+                return;
+            }
+            int mid = si + (ei - si) / 2; // .: (si + ei)/2 - this could lead to out of range for int if sum of both the values exceed the max value of int here.
+            divide(nums, si, mid);
+            divide(nums, mid + 1, ei);
+            // Conquor method to implement the sort logic and merge the array.
+            conquor(nums, si, mid, ei);
+
+        }
+
         public int[] sortArray(int[] nums) {
             int n = nums.length;
-            for (int i = 0; i < n - 1; i++) {
-                for (int j = 0; j < n - 1 - i; j++) {
-                    if (nums[j] > nums[j + 1]) {
-                        int temp = nums[j];
-                        nums[j] = nums[j + 1];
-                        nums[j + 1] = temp;
-                    }
-                }
-
-            }
+            divide(nums, 0, n - 1);
             return nums;
         }
     }
