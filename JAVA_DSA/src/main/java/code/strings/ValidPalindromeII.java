4ce438e71a46828026535742af6f8cdded4159c2
Rahul Arakka <arakkarahul11@gmail.com>
2024-08-20 00:21:29 +0530

    ValidPalindromeII - sample test cases passed | Leetcode | Strings - Easy

--- JAVA_DSA/src/main/java/code/strings/ValidPalindromeII.java
+++ JAVA_DSA/src/main/java/code/strings/ValidPalindromeII.java
@@ -2,32 +2,64 @@
 //https://leetcode.com/problems/valid-palindrome-ii/description/
 //Given a string s, return true if the s can be palindrome after deleting at most one character from it.
 
+// TLE
+
 package code.strings;
 
 public class ValidPalindromeII {
     class Solution {
         public boolean validPalindrome(String s) {
-            int i = 0, j = s.length() - 1;
-            while (i < j)
-                if (s.charAt(i) == s.charAt(j)) {
-                    i += 1;
-                    j -= 1;
-                } else
-                    return validPalindromeUtil(s, i + 1, j) || validPalindromeUtil(s, i, j - 1);
-            return true;
-        }
+            // String val = s.reverse();
+            String reversedStr = "";
 
-        boolean validPalindromeUtil(String s, int i, int j) {
-            while (i < j)
-                if (s.charAt(i) == s.charAt(j)) {
-                    i += 1;
-                    j -= 1;
-                } else {
-                    return false;
+            for (int i = 0; i < s.length(); i++) {
+                reversedStr = s.charAt(i) + reversedStr;
+            }
+            if (s.equals(reversedStr)) return true;
+
+            List<String> list = new ArrayList<>();
+            String[] arr = s.split("");
+
+            list = Arrays.asList(arr);
+            for (int i = 0; i < list.size(); i++) {
+                ArrayList<String> al = new ArrayList<>(list);
+                // al = list;
+                al.remove(i);
+                // String str = al.toString();
+                String str2 = "", str3 = "";
+
+                for (int j = 0; j < al.size(); j++) {
+                    str2 += al.get(j);
+                    str3 += al.get(al.size() - 1 - j);
                 }
+                // System.out.println("Printing s: "+s+" and reverse : "+ str2);
+                if (str2.equals(str3)) {
+                    return true;
+                }
+            }
+            return false;
+            //     int i = 0, j = s.length() - 1;
+            //     while(i < j)
+            //         if(s.charAt(i) == s.charAt(j)) {
+            //             i += 1;
+            //             j -= 1;
+            //         }else
+            //             return validPalindromeUtil(s, i + 1, j) || validPalindromeUtil(s, i, j - 1);
+            //     return true;
+            // }
 
-            return true;
+            // boolean validPalindromeUtil(String s, int i, int j) {
+            //     while(i < j)
+            //         if(s.charAt(i) == s.charAt(j)) {
+            //             i += 1;
+            //             j -= 1;
+            //         } else {
+            //             return false;
+            //         }
+
+            //     return true;
         }
     }
+
 
 }
