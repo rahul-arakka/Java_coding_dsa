f7df29422a06ba2d4c14ee0a93e5e22bef990a25
Rahul Arakka <arakkarahul11@gmail.com>
2024-08-20 23:44:23 +0530

    MoveZeros - All test cases passed | Leetcode | Array - Easy | Optimal approach

--- JAVA_DSA/src/main/java/code/arrays/MoveZeros.java
+++ JAVA_DSA/src/main/java/code/arrays/MoveZeros.java
@@ -3,19 +3,24 @@
 //Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 //Note that you must do this in-place without making a copy of the array.
 
+// This approach runs in O(n) time complexity and work for all test cases.
+
 package code.arrays;
 
 public class MoveZeros {
     class Solution {
         public void moveZeroes(int[] nums) {
+            int x = 0, count = 0;
             for (int i = 0; i < nums.length; i++) {
                 if (nums[i] == 0) {
-                    for (int j = i; j < nums.length - 1; j++) {
-                        int temp = nums[j];
-                        nums[j] = nums[j + 1];
-                        nums[j + 1] = temp;
-                    }
+                    count++;
+                    continue;
+                } else {
+                    nums[x++] = nums[i];
                 }
+            }
+            while (count > 0) {
+                nums[nums.length - count--] = 0;
             }
         }
     }
