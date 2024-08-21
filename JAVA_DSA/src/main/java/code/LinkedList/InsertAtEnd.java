c0d7b3d736c651b4e31a9670f46efaef8ffb0b4c
Rahul Arakka <arakkarahul11@gmail.com>
2024-08-21 17:01:00 +0530

    InsertAtEnd - All passes | Linked List Java - Easy | GFG

---
+++ JAVA_DSA/src/main/java/code/LinkedList/InsertAtEnd.java
@@ -0,0 +1,35 @@
+//https://www.geeksforgeeks.org/problems/linked-list-insertion-1587115620/1?page=1&category=Linked%20List&difficulty=Basic&sortBy=submissions
+//Given the head of a Singly Linked List and a value x, insert that value x at the end of the LinkedList and return the modified Linked List.
+
+package code.LinkedList;
+
+//Node is defined as
+class Node {
+    int data;
+    Node next;
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+}
+
+public class InsertAtEnd {
+    class Solution {
+        // Function to insert a node at the end of the linked list.
+        Node insertAtEnd(Node head, int x) {
+            // code here
+            Node node = new Node(x);
+            if (head == null) {
+                return node;
+            }
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+
+            temp.next = node;
+
+            return head;
+        }
+    }
+}
