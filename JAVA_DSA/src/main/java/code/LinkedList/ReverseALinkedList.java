83bed93eabf2971835f02d8a1c2a929a0751f222
Rahul Arakka <arakkarahul11@gmail.com>
2024-08-21 20:51:06 +0530

    ReverseALinkedList - All passed | LinkedList - Easy | GFG

---
+++ JAVA_DSA/src/main/java/code/LinkedList/ReverseALinkedList.java
@@ -0,0 +1,21 @@
+//https://www.geeksforgeeks.org/problems/reverse-a-linked-list/1?page=1&category=Linked%20List&difficulty=Easy&sortBy=submissions
+//Given the head of a linked list, the task is to reverse this list and return the reversed head.
+
+package code.LinkedList;
+
+public class ReverseALinkedList {
+    class Solution {
+        // Function to reverse a linked list.
+        Node reverseList(Node head) {
+            // code here
+            Node prev = null;
+            while (head != null) {
+                Node temp = head.next;
+                head.next = prev;
+                prev = head;
+                head = temp;
+            }
+            return prev;
+        }
+    }
+}
