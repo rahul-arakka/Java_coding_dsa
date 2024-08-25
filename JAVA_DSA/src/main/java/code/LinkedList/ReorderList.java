34340008921248ed7ac4bbbb1d0def16056b82a5
Rahul Arakka <arakkarahul11@gmail.com>
2024-08-25 17:09:12 +0530

    ReorderList - All cases passed | LeetCode | LL - Medium

--- JAVA_DSA/src/main/java/code/LinkedList/ReorderList.java
+++ JAVA_DSA/src/main/java/code/LinkedList/ReorderList.java
@@ -9,41 +9,42 @@
 //L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
 //You may not modify the values in the list's nodes. Only nodes themselves may be changed.
 
-// brute force approach - hit and try - first attempt towards the problem, need to be enhanced.
+// This approach works for all test cases, we first traverse to the middle of LL using slow & fast pointers (with fast = head.next).
+// Then reverse rest of the LL and then start pointing the next nodes accordingly.
 
 package code.LinkedList;
 
 public class ReorderList {
     class Solution {
         public void reorderList(ListNode head) {
-            ListNode result = new ListNode(0), h3 = result;
-            ListNode temp = head, slow = head, fast = head, prev = null;
+            ListNode slow = head, fast = head.next;
 
             while (fast != null && fast.next != null) {
-                prev = slow;
                 slow = slow.next;
                 fast = fast.next.next;
             }
-            prev.next = null;
-            prev = slow;
 
-            while (slow.next != null) {
-                System.out.println("printing slow val : " + slow.val);
-                ListNode t2 = slow.next;
-                slow.next = prev;
-                prev = slow;
-                slow = t2;
+            ListNode second = slow.next;
+            ListNode prev = slow.next = null;
+
+            while (second != null) {
+                // System.out.println("printing slow val : "+ slow.val);
+                ListNode t2 = second.next;
+                second.next = prev;
+                prev = second;
+                second = t2;
 
             }
-            ListNode t1 = head, t2 = slow;
-            while (head.next != null && slow.next != null) {
-                System.out.println("printing val: " + head.val + " slow val : " + slow.val);
-                t1 = head.next;
-                t2 = slow.next;
-                head.next = t1;
-                slow.next = t2;
+            // ListNode first;
+            second = prev;
+            while (second != null) {
+                // System.out.println("printing val: "+ head.val +" slow val : "+ slow.val);
+                ListNode t1 = head.next;
+                ListNode t2 = second.next;
+                head.next = second;
+                second.next = t1;
                 head = t1;
-                slow = t2;
+                second = t2;
             }
             return;
 
