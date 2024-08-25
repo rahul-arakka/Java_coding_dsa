da915534b3010e3b8417af50e9b60f5ef87d3d53
Rahul Arakka <arakkarahul11@gmail.com>
2024-08-25 22:17:37 +0530

    FindPairsWithGivenSum - Major cases passed | LeetCode | DLL - Easy | TLE

--- JAVA_DSA/src/main/java/code/LinkedList/DoublyLinkedList/FindPairsWithGivenSum.java
+++ JAVA_DSA/src/main/java/code/LinkedList/DoublyLinkedList/FindPairsWithGivenSum.java
@@ -1,3 +1,8 @@
+//https://www.geeksforgeeks.org/problems/find-pairs-with-given-sum-in-doubly-linked-list/1?page=1&category=doubly-linked-list&difficulty=Easy&sortBy=submissions
+//Given a sorted doubly linked list of positive distinct elements, the task is to find pairs in a doubly-linked list whose sum is equal to given value target.
+
+// This is optimized than the previous approach, here we are removing the nodes for which we already checked the sum to reduce the traversal.
+
 package code.LinkedList.DoublyLinkedList;
 
 import java.util.ArrayList;
@@ -7,7 +12,7 @@ public class FindPairsWithGivenSum {
         public static ArrayList<ArrayList<Integer>> findPairsWithGivenSum(int target, DLLNode head) {
             // code here
             ArrayList<ArrayList<Integer>> result = new ArrayList<>();
-            while (head.next != null) {
+            while (head != null) {
 
                 DLLNode tmp = head.next;
                 while (tmp != null) {
@@ -16,6 +21,18 @@ public class FindPairsWithGivenSum {
                         pair.add(head.data);
                         pair.add(tmp.data);
                         result.add(pair);
+
+
+                        if (tmp.next != null) {
+                            tmp.prev.next = tmp.next;
+                            tmp.next.prev = tmp.prev;
+                        } else {
+                            tmp.prev.next = null;
+                            tmp.next = null;
+
+                        }
+
+
                     }
                     tmp = tmp.next;
                 }
@@ -25,6 +42,4 @@ public class FindPairsWithGivenSum {
             return result;
         }
     }
-
-
 }
