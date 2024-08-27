15e3c61eb3a1c5e40942ccd860eabeea476570a5
Rahul Arakka <arakkarahul11@gmail.com>
2024-08-27 11:26:44 +0530

    BuddyStrings - All cases passed | optimal approach

--- JAVA_DSA/src/main/java/code/strings/BuddyStrings.java
+++ JAVA_DSA/src/main/java/code/strings/BuddyStrings.java
@@ -3,31 +3,50 @@
 //Swapping letters is defined as taking two indices i and j (0-indexed) such that i != j and swapping the characters at s[i] and s[j].
 //For example, swapping at indices 0 and 2 in "abcd" results in "cbad".
 
-// This approach is suitable for sample test cases, and edge cases are not covered.
+// This approach is suitable for All test cases, and edge cases are also covered.
+// Runs with 1ms & beats 98.64%
 
 package code.strings;
+
+import java.util.HashSet;
 
 public class BuddyStrings {
     class Solution {
         public boolean buddyStrings(String s, String goal) {
-            // String[]
-            for (int i = 0; i < s.length(); i++) {
 
-                for (int j = 0; j < s.length(); j++) {
-                    StringBuilder str = new StringBuilder(s);
-                    if (i == j) continue;
-                    char c = str.charAt(i);
-                    str.setCharAt(i, str.charAt(j));
-                    str.setCharAt(j, c);
-                    String s2 = str.toString();
-                    // System.out.println("printing str : "+s2);
-                    if (goal.equals(s2)) {
+            if (s.length() != goal.length()) {
+                return false;
+            }
+            HashSet<Character> set = new HashSet<>();
+
+            if (s.equals(goal)) {
+                for (char ch : s.toCharArray()) {
+                    if (!set.add(ch)) {
                         return true;
                     }
-
                 }
+                return false;
             }
-            return false;
+
+            int diff = 0;
+            Character sPrev = null;
+            Character gPrev = null;
+            for (int i = 0; i < s.length(); i++) {
+                char s1 = s.charAt(i);
+                char g1 = goal.charAt(i);
+
+                if (s1 != g1) {
+                    if (diff == 2 || diff == 1 && (s1 != gPrev || g1 != sPrev)) {
+                        return false;
+                    }
+
+                    sPrev = s1;
+                    gPrev = g1;
+                    diff++;
+                }
+
+            }
+            return diff == 2;
         }
     }
 }
