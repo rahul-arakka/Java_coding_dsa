0c5750ff02e2a706af07000d237741b80a9810da
Rahul Arakka <arakkarahul11@gmail.com>
2024-08-28 21:45:03 +0530

    PivotNumber - All cases passed | Math - Easy | LeetCode | Second approach

--- JAVA_DSA/src/main/java/code/Math/PivotNumber.java
+++ JAVA_DSA/src/main/java/code/Math/PivotNumber.java
@@ -3,25 +3,29 @@
 //The sum of all elements between 1 and x inclusively equals the sum of all elements between x and n inclusively.
 //Return the pivot integer x. If no such integer exists, return -1. It is guaranteed that there will be at most one pivot index for the given input.
 
+// In this we are using sum of N numbers approach.
+
 package code.Math;
 
 public class PivotNumber {
     class Solution {
         public int pivotInteger(int n) {
-            int[] arr = new int[n];
-            int sum = 0;
 
-            for (int i = 0; i < n; i++) {
-                sum += i + 1;
-                arr[i] = sum;
-            }
-            int revSum = 0;
-            while (n > 0) {
-                revSum += n;
-                if (arr[n - 1] == revSum) {
-                    return n;
-                }
-                n--;
+            int i = 0;
+            while (i < n) {
+                int a = i + 1, d = 1, N = i + 1;
+                int sum1 = ((N) * (2 + (N - 1) * d)) / 2;
+
+                a = n;
+                d = -1;
+                N = n - i;
+                int sum2 = ((N) * (2 * a + (N - 1) * d)) / 2;
+                // System.out.println("printing sum1 "+ sum1 +" printing sum2 :" + sum2);
+
+                if (sum1 == sum2)
+                    return i + 1;
+                i++;
+
             }
             return -1;
         }
