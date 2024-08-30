c737932be150086bb74a2d4abe31531d8d8cb3b3
Rahul Arakka <arakkarahul11@gmail.com>
2024-08-30 23:24:04 +0530

    DayOfYear - sample cases passed | Math - Easy | LeetCode

---
+++ JAVA_DSA/src/main/java/code/Math/DayOfYear.java
@@ -0,0 +1,30 @@
+//https://leetcode.com/problems/day-of-the-year/description/
+//Given a string date representing a Gregorian calendar date formatted as YYYY-MM-DD, return the day number of the year.
+
+
+package code.Math;
+
+public class DayOfYear {
+    class Solution {
+        public int dayOfYear(String date) {
+            int day = 0;
+            String[] dt = date.split("-");
+            int month = Integer.parseInt(dt[1]);
+            for (int i = 1; i < month; i++) {
+                if (i == 2) {
+                    day += 28;
+                } else if (i == 4 || i == 6 || i == 9 || i == 11) {
+                    day += 30;
+                } else {
+                    day += 31;
+                }
+            }
+            day += Integer.parseInt(dt[2]);
+            int year = Integer.parseInt(dt[0]);
+            if (year % 4 == 0) {
+                day++;
+            }
+            return day;
+        }
+    }
+}
