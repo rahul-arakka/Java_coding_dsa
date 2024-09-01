57839027f04e77c7b3974d90c45aa413cd72a115
rahul-arakka <arakkarahul11@gmail.com>
2024-09-01 21:27:29 +0530

    NumberOfSegment - Sample test cases passed | Leetcode

---
+++ JAVA_DSA/src/main/java/code/strings/NumberOfSegment.java
@@ -0,0 +1,15 @@
+//https://leetcode.com/problems/number-of-segments-in-a-string/
+//Given a string s, return the number of segments in the string.
+//A segment is defined to be a contiguous sequence of non-space characters.
+
+package code.strings;
+
+public class NumberOfSegment {
+    class Solution {
+        public int countSegments(String s) {
+            if(s.length() == 0)   return 0;
+            String[] split = s.split(" ");
+            return split.length;
+        }
+    }
+}
