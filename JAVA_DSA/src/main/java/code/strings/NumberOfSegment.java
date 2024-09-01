51dacad01b74e1b490d196fd30a86916f4e3b71f
rahul-arakka <arakkarahul11@gmail.com>
2024-09-01 22:55:10 +0530

    NumberOfSegment - All test cases passed | Leetcode | Approach 2 | beats 100%

--- JAVA_DSA/src/main/java/code/strings/NumberOfSegment.java
+++ JAVA_DSA/src/main/java/code/strings/NumberOfSegment.java
@@ -8,9 +8,20 @@ public class NumberOfSegment {
     class Solution {
         public int countSegments(String s) {
             s = s.trim();
-            if (s.length() == 0) return 0;
-            String[] spl = s.split("\\s+");
-            return spl.length;
+            int len = 0;
+            boolean flag = false;
+            for(int i=0;i<s.length()-1;i++){
+                if(s.charAt(i) != s.charAt(i+1) && s.charAt(i) == ' '){
+                    flag = true;
+                    len++;
+                }
+
+            }
+
+            if(flag)    len++;
+            if(!flag && s.length() != 0) return 1;
+
+            return len;
         }
     }
 }
