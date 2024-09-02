61627d46a626b6ca289063cac0a84646c89136a2
Rahul Arakka <arakkarahul11@gmail.com>
2024-09-03 00:30:45 +0530

    MaximumCardPoints - Using recursion - Sample test cases passed| Arrays - Medium | LeetCode

--- JAVA_DSA/src/main/java/code/arrays/MaximumCardPoints.java
+++ JAVA_DSA/src/main/java/code/arrays/MaximumCardPoints.java
@@ -7,30 +7,36 @@
 //
 //Given the integer array cardPoints and the integer k, return the maximum score you can obtain.
 
-// This is brute force approach and works for sample test cases. | Array - Medium.
+// This is Recursion approach and works for sample test cases. | TLE | Array - Medium.
 
 package code.arrays;
 
 public class MaximumCardPoints {
     class Solution {
+        public int cardPoint(int[] a, int i, int j, int sum, int k) {
+            if (k == 0) {
+                return sum;
+            }
+
+            sum += a[i];
+            int sum1 = cardPoint(a, i + 1, j, sum, k - 1);
+            sum = sum + a[j] - a[i];
+            // System.out.println("i: "+i+" J :"+j+" Sum : "+ sum);
+            int sum2 = cardPoint(a, i, j - 1, sum, k - 1);
+            return Math.max(sum1, sum2);
+        }
+
         public int maxScore(int[] cardPoints, int k) {
             int sum = 0;
             int i = 0, j = cardPoints.length - 1;
-            while (k > 0) {
-                if (cardPoints[i] == cardPoints[j]) {
-                    sum += cardPoints[i];
-                    i++;
-                    j--;
-                } else if (cardPoints[i] > cardPoints[j]) {
-                    sum += cardPoints[i];
-                    i++;
-                } else {
-                    sum += cardPoints[j];
-                    j--;
+            if (k == cardPoints.length) {
+                for (int x = 0; x < cardPoints.length; x++) {
+                    sum += cardPoints[x];
                 }
-                k--;
+                return sum;
             }
-            return sum;
+            return cardPoint(cardPoints, i, j, sum, k);
+            // return ;
         }
     }
 }
