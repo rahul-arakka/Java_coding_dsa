e0b739fbee187a5b45eb7ac2836edb25bc1ee757
Rahul Arakka <arakkarahul11@gmail.com>
2024-09-03 01:32:41 +0530

    MaximumCardPoints - approach 4 | optimal approach | All test cases passed | Array - medium

--- JAVA_DSA/src/main/java/code/arrays/MaximumCardPoints.java
+++ JAVA_DSA/src/main/java/code/arrays/MaximumCardPoints.java
@@ -13,28 +13,24 @@ package code.arrays;
 
 public class MaximumCardPoints {
     class Solution {
-
-        public int maxScore(int[] cardPoints, int k) {
-            int n = cardPoints.length, sum = 0;
-            if (n == k) {
-                for (int i = 0; i < n; i++) {
-                    sum += cardPoints[i];
-                }
-                return sum;
+        public int maxScore(int[] nums, int k) {
+            int n = nums.length;
+            int lsum = 0;
+            int rsum = 0;
+            int maxsum = 0;
+            for (int i = 0; i < k; i++) {
+                lsum = lsum + nums[i];
             }
-            int max = 0;
-            int start = k;
-            for (int i = 0; i < k + 1; i++) {
-                sum = 0;
-                for (int j = start - 1; j >= start - k; j--) {
-                    int x = (j + n) % n;
-                    sum += cardPoints[x];
-                }
-                max = Math.max(sum, max);
-                start--;
-
+            maxsum = lsum;
+            int rind = n - 1;
+            for (int i = k - 1; i >= 0; i--) {
+                lsum = lsum - nums[i];
+                rsum = rsum + nums[rind];
+                maxsum = Math.max(maxsum, lsum + rsum);
+                rind--;
             }
-            return max;
+            return maxsum;
+
         }
     }
 }
