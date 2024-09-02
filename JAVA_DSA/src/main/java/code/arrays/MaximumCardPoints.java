328b553e4955a4599e56b9e5bbd6485efc7fec42
Rahul Arakka <arakkarahul11@gmail.com>
2024-09-03 01:28:46 +0530

    MaximumCardPoints - approach 3

--- JAVA_DSA/src/main/java/code/arrays/MaximumCardPoints.java
+++ JAVA_DSA/src/main/java/code/arrays/MaximumCardPoints.java
@@ -13,30 +13,28 @@ package code.arrays;
 
 public class MaximumCardPoints {
     class Solution {
-        public int cardPoint(int[] a, int i, int j, int sum, int k) {
-            if (k == 0) {
-                return sum;
-            }
-
-            sum += a[i];
-            int sum1 = cardPoint(a, i + 1, j, sum, k - 1);
-            sum = sum + a[j] - a[i];
-            // System.out.println("i: "+i+" J :"+j+" Sum : "+ sum);
-            int sum2 = cardPoint(a, i, j - 1, sum, k - 1);
-            return Math.max(sum1, sum2);
-        }
 
         public int maxScore(int[] cardPoints, int k) {
-            int sum = 0;
-            int i = 0, j = cardPoints.length - 1;
-            if (k == cardPoints.length) {
-                for (int x = 0; x < cardPoints.length; x++) {
-                    sum += cardPoints[x];
+            int n = cardPoints.length, sum = 0;
+            if (n == k) {
+                for (int i = 0; i < n; i++) {
+                    sum += cardPoints[i];
                 }
                 return sum;
             }
-            return cardPoint(cardPoints, i, j, sum, k);
-            // return ;
+            int max = 0;
+            int start = k;
+            for (int i = 0; i < k + 1; i++) {
+                sum = 0;
+                for (int j = start - 1; j >= start - k; j--) {
+                    int x = (j + n) % n;
+                    sum += cardPoints[x];
+                }
+                max = Math.max(sum, max);
+                start--;
+
+            }
+            return max;
         }
     }
 }
