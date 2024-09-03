3db96227b713b0529ed23080c932deaad1c90109
Rahul Arakka <arakkarahul11@gmail.com>
2024-09-03 23:27:10 +0530

    JumpGame - Optimal approach | All test cases passed | Array - medium

--- JAVA_DSA/src/main/java/code/arrays/JumpGame.java
+++ JAVA_DSA/src/main/java/code/arrays/JumpGame.java
@@ -7,22 +7,13 @@ package code.arrays;
 
 public class JumpGame {
     class Solution {
-        public boolean func(int[] a, int i, int n) {
-            if (i == n)
-                return true;
-            if (a[i] == 0 || i > n)
-                return false;
-
-            return func(a, a[i] + i, n);
-            // boolean bool1 = func(a, a[i]+i, n);
-            // boolean bool2 = func(a, a[i]+i-1, n);
-            // return bool1 || bool2;
-        }
-
         public boolean canJump(int[] nums) {
-            int i = 0, n = nums.length;
-            boolean result = func(nums, i, n - 1);
-            return result;
+            int reach = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (i > reach) return false;
+                reach = Math.max(reach, i + nums[i]);
+            }
+            return true;
         }
     }
 }
