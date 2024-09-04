e565c65ee3bcdb297b19f16d905935a6c43c5c9a
Rahul Arakka <arakkarahul11@gmail.com>
2024-09-04 23:07:53 +0530

    ValidSudoku - brute force approach | sample test cases passed | Array - medium

---
+++ JAVA_DSA/src/main/java/code/arrays/ValidSudoku.java
@@ -0,0 +1,48 @@
+//https://leetcode.com/problems/valid-sudoku/description/?envType=problem-list-v2&envId=array
+//Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+//
+//Each row must contain the digits 1-9 without repetition.
+//Each column must contain the digits 1-9 without repetition.
+//Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+//Note:
+//
+//A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+//Only the filled cells need to be validated according to the mentioned rules.
+
+package code.arrays;
+
+public class ValidSudoku {
+    class Solution {
+        public boolean isValidSudoku(char[][] board) {
+            // Traversing at each ith column and check for duplicates.
+            for (int i = 0; i < board.length; i++) {
+                Map<Character, Integer> mp = new HashMap<>();
+                for (int j = 0; j < board.length; j++) {
+                    if (board[i][j] == '.')
+                        continue;
+
+                    if (mp.getOrDefault(board[i][j], 0) > 0) {
+                        System.out.println(board[i][j] + " : " + i + " : " + j);
+                        return false;
+                    }
+                    mp.put(board[i][j], 1);
+                }
+            }
+            // Traversing at each jth row and check for duplicates.
+            for (int i = 0; i < board.length; i++) {
+                Map<Character, Integer> mp = new HashMap<>();
+                for (int j = 0; j < board.length; j++) {
+                    if (board[j][i] == '.')
+                        continue;
+
+                    if (mp.getOrDefault(board[j][i], 0) > 0) {
+                        System.out.println(board[j][i] + " : " + i + " : " + j);
+                        return false;
+                    }
+                    mp.put(board[j][i], 1);
+                }
+            }
+            return true;
+        }
+    }
+}
