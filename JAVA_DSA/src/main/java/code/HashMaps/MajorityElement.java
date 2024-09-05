14d99de57d99ac6038bdeffa0ba15a4c04a011e4
Rahul Arakka <arakkarahul11@gmail.com>
2024-09-05 23:19:46 +0530

    MajorityElement - All test cases passed | approach 1 | HashMaps - Easy

---
+++ JAVA_DSA/src/main/java/code/HashMaps/MajorityElement.java
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/majority-element/description/?envType=problem-list-v2&envId=hash-table
+//Given an array nums of size n, return the majority element.
+//
+//The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+package code.HashMaps;
+
+public class MajorityElement {
+    class Solution {
+        public int majorityElement(int[] nums) {
+            Map<Integer, Integer> map = new HashMap<>();
+            int max = 0;
+            int res = 0;
+            for (int val : nums) {
+                map.put(val, map.getOrDefault(val, 0) + 1);
+                if ((int) map.get(val) > max) {
+                    max = (int) map.get(val);
+                    res = val;
+                }
+
+            }
+            return res;
+        }
+    }
+}
