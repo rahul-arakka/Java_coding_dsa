62b406a950dd0d018f6f9dbe112d41f4679d1ca9
Rahul Arakka <arakkarahul11@gmail.com>
2024-09-06 23:31:54 +0530

    MergeSortedArray - All test cases passed | Arrays - Easy | Optimal approach

--- JAVA_DSA/src/main/java/code/arrays/MergeSortedArray.java
+++ JAVA_DSA/src/main/java/code/arrays/MergeSortedArray.java
@@ -5,7 +5,7 @@
 //
 //The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 
-// This approach works for all test cases with 1ms & beats 36.25%
+// This approach works for all test cases with 0ms & beats 100.00%
 
 package code.arrays;
 
@@ -14,10 +14,17 @@ import java.util.Arrays;
 public class MergeSortedArray {
     class Solution {
         public void merge(int[] nums1, int m, int[] nums2, int n) {
-            for (int i = m; i < m + n; i++) {
-                nums1[i] = nums2[i - m];
+            int i = m - 1;
+            int j = n - 1;
+            int k = m + n - 1;
+
+            while (j >= 0) {
+                if (i >= 0 && nums1[i] > nums2[j]) {
+                    nums1[k--] = nums1[i--];
+                } else {
+                    nums1[k--] = nums2[j--];
+                }
             }
-            Arrays.sort(nums1);
         }
     }
 }
