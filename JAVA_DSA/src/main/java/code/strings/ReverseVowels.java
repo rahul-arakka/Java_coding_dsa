caeae3b16d91fa68f82e0e15b73b670c55789aed
Rahul Arakka <arakkarahul11@gmail.com>
2024-09-07 23:28:14 +0530

    ReverseVowels - important comments updated

--- JAVA_DSA/src/main/java/code/strings/ReverseVowels.java
+++ JAVA_DSA/src/main/java/code/strings/ReverseVowels.java
@@ -2,33 +2,34 @@
 //Given a string s, reverse only all the vowels in the string and return it.
 //
 //The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
-// This approach works for all test cases with 255ms & beats 5.10%
+// This approach works for all test cases with 5ms & beats 39.16%
 
 package code.strings;
 
 public class ReverseVowels {
     class Solution {
         public String reverseVowels(String s) {
-            String[] str = s.split("");
+            char[] str = s.toCharArray();
             String vowel = "aeiouAEIOU";
             int i = 0, j = str.length - 1;
             while (i < j) {
-                if (!vowel.contains(str[i])) {
-                    i++;
-                } else if (!vowel.contains(str[j])) {
+                if (!vowel.contains(String.valueOf(str[i]))) {      //As we are using inbuilt too much functions here like .contains() & String.valueOf() so the execution is little slower.
+                    i++;                                            // Instead of calling functions, if we directly check the characters, it will be faster.
+                } else if (!vowel.contains(String.valueOf(str[j]))) {
                     j--;
                 } else {
-                    String tmp = str[i];
+                    char tmp = str[i];
                     str[i] = str[j];
                     str[j] = tmp;
                     i++;
                     j--;
                 }
             }
-            String result = "";
-            for (String val : str)
-                result += val;
-            return result;
+            // String result = "";      :- As we were using an additional for loop the program runs an extra space and reduces performance
+            // for(char val : str)
+            //     result += String.valueOf(val);
+            return new String(str); // Using direct new string() will be faster, but this only work if the provided array is Char array and does not work in case of string array.
+            // return String.valueOf(str)   :- alternative of above command, but works same way.
         }
     }
 }
