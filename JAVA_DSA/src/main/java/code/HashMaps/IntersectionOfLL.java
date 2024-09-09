9e6abc91a9adb97e92ef40b4399919885012ddb3
Rahul Arakka <arakkarahul11@gmail.com>
2024-09-09 23:19:16 +0530

    IntersectionOfLL - optimal appraoch | HashMap - Easy

--- JAVA_DSA/src/main/java/code/HashMaps/IntersectionOfLL.java
+++ JAVA_DSA/src/main/java/code/HashMaps/IntersectionOfLL.java
@@ -19,20 +19,19 @@ public class IntersectionOfLL {
 
     public class Solution {
         public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-            // Map<ListNode, Integer> map = new HashMap<>();
-//            List<ListNode> list = new ArrayList<>();
-            while(headA != null){
-                ListNode tmp = headB;
-                while(tmp != null){
-                    if(tmp == headA){
-                        return tmp;
-                    }
-                    tmp = tmp.next;
-                }
-                headA = headA.next;
+            if(headA == null || headB == null) return null;
+
+            ListNode a = headA;
+            ListNode b = headB;
+
+            //if a & b have different len, then we will stop the loop after second iteration
+            while( a != b){
+                //for the end of first iteration, we just reset the pointer to the head of another linkedlist
+                a = a == null? headB : a.next;
+                b = b == null? headA : b.next;
             }
 
-            return null;
+            return a;
         }
     }
 }
