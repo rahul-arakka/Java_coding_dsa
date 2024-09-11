8da3a6f323ca16396bbf5b67fc1a0d6a0c7d682d
Rahul Arakka <arakkarahul11@gmail.com>
2024-09-11 23:39:32 +0530

    IslandPerimeter - All test cases passed | Approach - 2

--- JAVA_DSA/src/main/java/code/arrays/IslandPerimeter.java
+++ JAVA_DSA/src/main/java/code/arrays/IslandPerimeter.java
@@ -10,43 +10,26 @@ package code.arrays;
 public class IslandPerimeter {
     class Solution {
         public int islandPerimeter(int[][] grid) {
-            int rows = grid.length;
-            int cols = grid[0].length;
-            int ans = 0;
-            int up, down, left, right;
-
-            for (int r = 0; r < rows; r++) {
-                for (int c = 0; c < cols; c++) {
-                    if (grid[r][c] == 1) {
-                        if (r == 0) {
-                            up = 1;
-                        } else {
-                            up = (grid[r - 1][c] == 0) ? 1 : 0;
+            int count=0;
+            for(int i=0; i<grid.length; i++){
+                for(int j=0; j<grid[0].length; j++){
+                    if(grid[i][j]==1){
+                        if(i-1==-1 || grid[i-1][j]==0){
+                            count++;
                         }
-
-                        if (c == 0) {
-                            left = 1;
-                        } else {
-                            left = (grid[r][c - 1] == 0) ? 1 : 0;
+                        if(j-1==-1 || grid[i][j-1]==0){
+                            count++;
                         }
-
-                        if (r == rows - 1) {
-                            down = 1;
-                        } else {
-                            down = (grid[r + 1][c] == 0) ? 1 : 0;
+                        if(i+1==grid.length || grid[i+1][j]==0){
+                            count++;
                         }
-
-                        if (c == cols - 1) {
-                            right = 1;
-                        } else {
-                            right = (grid[r][c + 1] == 0) ? 1 : 0;
+                        if(j+1==grid[0].length || grid[i][j+1]==0){
+                            count++;
                         }
-
-                        ans += up + left + down + right;
                     }
                 }
             }
-            return ans;
+            return count;
         }
     }
 
