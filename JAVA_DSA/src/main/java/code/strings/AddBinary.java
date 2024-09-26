dc905e9c2adcc66d61d4ff3a924780c15004d597
rahul-arakka <arakkarahul11@gmail.com>
2024-09-26 23:23:30 +0530

    AddBinary - All test cases passed | Strings - Easy | Approach 2

--- JAVA_DSA/src/main/java/code/strings/AddBinary.java
+++ JAVA_DSA/src/main/java/code/strings/AddBinary.java
@@ -3,41 +3,19 @@
 package code.strings;
 
 public class AddBinary {
-    class Solution {
-        public String addBinary(String x, String y) {
-//            int num1 = Integer.parseInt(a, 2);
-//            int num2 = Integer.parseInt(b, 2);
-//            int res = num1 + num2;
-//            return Integer.toBinaryString(res);
-            int i = x.length() - 1, j = y.length() - 1;
-            int carry = 0;
-            StringBuilder result = new StringBuilder();
+    public class Solution {
+        public String addBinary(String a, String b) {
+            StringBuilder sb = new StringBuilder();
+            int i = a.length() - 1, j = b.length() - 1, carry = 0;
             while (i >= 0 || j >= 0) {
                 int sum = carry;
-                if (i >= 0) {
-                    sum += x.charAt(i) - '0';
-                }
-                if (j >= 0) {
-                    sum += y.charAt(j) - '0';
-                }
-                if (sum == 0 || sum == 1) {
-                    result.append(sum);
-                    carry = 0;
-                } else if (sum == 2) {
-                    result.append("0");
-                    carry = 1;
-                } else {
-                    result.append("1");
-                    carry = 1;
-                }
-                i--;
-                j--;
+                if (j >= 0) sum += b.charAt(j--) - '0';
+                if (i >= 0) sum += a.charAt(i--) - '0';
+                sb.append(sum % 2);
+                carry = sum / 2;
             }
-            if (carry == 1) {
-                result.append("1");
-            }
-            return result.reverse().toString();
-
+            if (carry != 0) sb.append(carry);
+            return sb.reverse().toString();
         }
     }
 }
