7525a11eaf79993a763443e05aa4c2c721d4726b
rahul-arakka <arakkarahul11@gmail.com>
2024-09-27 19:17:34 +0530

    LiscenseKeyFormatting - All test cases passed | Strings - Easy | Approach 1

---
+++ JAVA_DSA/src/main/java/code/strings/LiscenseKeyFormatting.java
@@ -0,0 +1,34 @@
+//https://leetcode.com/problems/license-key-formatting/description/?envType=problem-list-v2&envId=string&difficulty=EASY
+//You are given a license key represented as a string s that consists of only alphanumeric characters and dashes. The string is separated into n + 1 groups by n dashes. You are also given an integer k.
+//
+//We want to reformat the string s such that each group contains exactly k characters, except for the first group, which could be shorter than k but still must contain at least one character. Furthermore, there must be a dash inserted between two groups, and you should convert all lowercase letters to uppercase.
+//
+//Return the reformatted license key.
+
+package code.strings;
+
+public class LiscenseKeyFormatting {
+    class Solution {
+        public String licenseKeyFormatting(String s, int k) {
+            char[] ch = s.toCharArray();
+            int n = ch.length;
+            String res = "";
+            int count = 0;
+            for (int i = ch.length - 1; i >= 0; i--) {
+                if (ch[i] == '-') {
+                    continue;
+                }
+                if (count == k) {
+                    res = "-" + res;
+                    count = 0;
+                }
+                count++;
+                if (ch[i] >= 90 && ch[i] <= 122)
+                    res = String.valueOf(ch[i]).toUpperCase() + res;
+                else res = String.valueOf(ch[i]) + res;
+            }
+
+            return res;
+        }
+    }
+}
