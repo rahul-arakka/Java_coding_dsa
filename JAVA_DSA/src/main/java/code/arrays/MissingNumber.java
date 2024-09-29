cec99dec5ce30522fd44e0e701375787f3b5fa50
rahul-arakka <arakkarahul11@gmail.com>
2024-09-29 15:49:56 +0530

    MissingNumber - All test cases passed |Sorting - Easy | LeetCode | Approach 2 | Optimized solution - comments

--- JAVA_DSA/src/main/java/code/arrays/MissingNumber.java
+++ JAVA_DSA/src/main/java/code/arrays/MissingNumber.java
@@ -2,23 +2,27 @@
 //Given an array nums containing n distinct numbers in the range [0, n],
 // return the only number in the range that is missing from the array.
 
+//This approach is the better choice due to its superior time complexity (O(n) vs. Previous approach which is using Array sort() method having time complexity O(n log n)),
+//while both have similar space complexity (O(1)).
+//Using XOR to find the missing number is a more efficient algorithm than sorting the array first.
 
 package code.arrays;
 
 import java.util.Arrays;
 
 public class MissingNumber {
-    class Solution {
+    public class Solution {
         public int missingNumber(int[] nums) {
-            Arrays.sort(nums);
-            int i = 0;
-            while(i<nums.length){
-                if(nums[i] != i++){
-                    return i-1;
-                }
+            int n = nums.length;
+            int ans = 0;
+            for (int i = 1; i <= n; i++) {
+                ans = ans ^ i;
             }
-
-            return i;
+            for (int i = 0; i < nums.length; i++) {
+                ans = ans ^ nums[i];
+            }
+            return ans;
         }
     }
+
 }
